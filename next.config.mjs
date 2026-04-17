21a4eb3cc699a080dc855a80da050a681c99d179
lvermeire <loic.vermeire@gmail.com>
2026-04-17 16:31:57 +0200

    update to pnpm

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
+const basePath = process.env.NEXT_BASE_PATH && process.env.NEXT_BASE_PATH !== "/" ? process.env.NEXT_BASE_PATH : ""
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath,
+  assetPrefix: basePath || undefined,
   typescript: {
     ignoreBuildErrors: true,
   },
